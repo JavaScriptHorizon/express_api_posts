9db2ce8642b32bd64445ff8d142b0bb4ec5b209d
mohamed Walid <javascripthorizon@gmail.com>
2026-06-19 04:35:28 +0300

    v1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const exp = require('express');
+const redoc = require("redoc-express");
 const cors = require('cors');
 const path = require("path");
 const app = exp();
@@ -17,6 +18,18 @@ app.use(cors({
 app.use(exp.json());
 app.use(exp.static(path.join(__dirname, "/public")));
 app.use("/api", require("./routes/index"));
+// Serve OpenAPI file
+app.get("/openapi.json", (req, res) => {
+    res.sendFile(path.join(__dirname, "openapi.json"));
+});
 
+// ReDoc UI
+app.get(
+    "/docs",
+    redoc({
+        title: "My API Documentation",
+        specUrl: "/openapi.json",
+    })
+);
 
 module.exports = app;
